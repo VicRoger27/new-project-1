37ffd4a5936ac8a8291d9b0ef736d80c4b1980cd
VicRoger27 <121492850+VicRoger27@users.noreply.github.com>
2023-02-17 19:49:47 +0000

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,12 +1,30 @@
 input.onButtonPressed(Button.A, function () {
-    radio.sendString("123")
+    radio.raiseEvent(
+    EventBusSource.MICROBIT_ID_BUTTON_A,
+    EventBusValue.MICROBIT_BUTTON_EVT_CLICK
+    )
+    basic.showString("A")
+})
+input.onButtonPressed(Button.AB, function () {
+    basic.showString("c")
+    radio.raiseEvent(
+    EventBusSource.MICROBIT_ID_BUTTON_AB,
+    EventBusValue.MICROBIT_BUTTON_EVT_CLICK
+    )
 })
 radio.onReceivedString(function (receivedString) {
-    basic.showString(receivedString)
+    for (let index = 0; index < 3; index++) {
+        basic.showString(receivedString)
+    }
 })
+input.onButtonPressed(Button.B, function () {
+    radio.raiseEvent(
+    EventBusSource.MICROBIT_ID_BUTTON_B,
+    EventBusValue.MICROBIT_BUTTON_EVT_CLICK
+    )
+    basic.showString("B")
+})
+led.enable(true)
 music.setBuiltInSpeakerEnabled(false)
 radio.setTransmitSerialNumber(true)
 radio.setGroup(1)
-basic.forever(function () {
-	
-})
